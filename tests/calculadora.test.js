4181958fa35e2cd0cfac9baec1edf1a9e4c03bc9
Vagner Bezerra <spotify.vagner@gmail.com>
2023-05-11 20:19:54 -0300

    commit

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -4,6 +4,12 @@ const calculadora = require('./../src/calculadora')
 test('A soma de 1 e 2 deve ser igual a 3', () => {
 
     //expect(sum(1, 2)).toBe(3);
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('A soma de 1 e 2 não deve ser igual a 5', () => {
+
+    //expect(sum(1, 2)).toBe(3);
     expect(sum(1, 2)).not.toBe(5);
 });
 
@@ -13,15 +19,33 @@ test('A diferenca de 10 e 2 deve ser igual a 8', () => {
 
 });
 
+test('A diferenca de 10 e 2 não deve ser igual a 5', () => {
+
+    expect(diff(10, 2)).not.toBe(5);
+
+});
+
 test('A multiplicacao de 3 e 2 deve ser igual a 6', () => {
 
     expect(product(3, 2)).toBe(6);
 
 });
 
+test('A multiplicacao de 3 e 2 não deve ser igual a 5', () => {
+
+    expect(product(3, 2)).not.toBe(5);
+
+});
+
 test('A divisao de 10 e 2 deve ser igual a 5', () => {
 
     expect(divi(10, 2)).toBe(5);
+
+});
+
+test('A divisao de 10 e 2 não deve ser igual a 8', () => {
+
+    expect(divi(10, 2)).not.toBe(8);
 
 });
 
